0cfb54505b6b2baa69c2e37ab86574fdeb486693
Dzianis Dashkevich <dskecse@gmail.com>
2014-11-28 11:37:11 +0300

    Add an example on routing * run it w/ `ruby -run -e httpd . -p 8000` * alternatively run w/ `python -m SimpleHTTPServer`

--- sect4/app/app.js
+++ sect4/app/app.js
@@ -1,3 +1,12 @@
 (function() {
-  angular.module('customersApp', ['ngRoute']);
+  var app = angular.module('customersApp', ['ngRoute']);
+
+  app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        controller:  'CustomersController',
+        templateUrl: 'app/views/customers.html'
+      })
+      .otherwise({ redirectTo: '/' });
+  }]);
 }());
