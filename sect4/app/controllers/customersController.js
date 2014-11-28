381f618eca62d3fad234a6964e7a195d9c248147
Dzianis Dashkevich <dskecse@gmail.com>
2014-11-28 09:11:42 +0300

    Add an ngRoute module as a dependency

---
+++ sect4/app/controllers/customersController.js
@@ -0,0 +1,15 @@
+angular.module('customersApp')
+  .controller('CustomersController', ['$scope', function ($scope) {
+    $scope.sortBy  = 'name';
+    $scope.reverse = false;
+
+    $scope.customers = [
+      { joined: '2000-12-02', name: 'John', city: 'Chandler',  orderTotal: 9.9956 },
+      { joined: '1965-01-25', name: 'Zed',  city: 'Las Vegas', orderTotal: 19.99 }
+    ]
+
+    $scope.doSort = function(propName) {
+      $scope.sortBy = propName;
+      $scope.reverse = !$scope.reverse;
+    }
+  }]);
